f1f282556156fe7694a04edf3f9bf1f8eae03388
Pamera-png <pameranatali@gmail.com>
2024-10-22 17:21:41 -0300

    teste inserir conta realizado com sucesso

---
+++ cypress/integration/features/account.feature
@@ -0,0 +1,6 @@
+#language: pt 
+Funcionalidade: inserirConta
+    Contexto: Realizar login com sucesso
+Dado que realizo login com sucesso
+Cenário: inserir conta com sucesso
+Quando insiro uma conta com sucesso
---
+++ cypress/support/business/account/accountSteps.js
@@ -0,0 +1,14 @@
+import { Given, Then, When, And } from "cypress-cucumber-preprocessor/steps"
+import AccountLogic from '../account/accountLogic';
+
+When('insiro uma conta com sucesso', ()=>{
+AccountLogic.inserirConta()
+})
+
+And('', () =>{
+
+})
+
+Then('',()=>{
+
+})
