33d420bc920d946cf9051dc773aa901d2a221fe3
Pamera-png <pameranatali@gmail.com>
2024-10-24 18:38:02 -0300

    teste criar conta com mesmo nome realizado com sucesso

--- cypress/integration/features/account.feature
+++ cypress/integration/features/account.feature
@@ -9,3 +9,6 @@ Quando insiro uma conta com sucesso
 
 Cenário: alterar conta com sucesso
 Quando altero uma conta com sucesso
+
+Cenário: criar conta com mesmo nome
+Quando crio uma conta com o mesmo nome
--- cypress/support/business/account/accountModel.js
+++ cypress/support/business/account/accountModel.js
@@ -1,6 +1,7 @@
 let nomeConta="Conta via rest"
 let token='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDA3NTh9.IPO0hq3HRiDPjHkGAaf7ns3oJTPPNb2BRQqKNWS-4fc'
 let contaByName="Conta para alterar"
+let contaRepetida="Conta mesmo nome"
 
 class AccountModel{
 getNomeConta(){
@@ -13,6 +14,10 @@ getToken(){
 
 getContaByName(){
     return contaByName
+}
+
+getContaRepetida(){
+    return contaRepetida
 }
 
 }
