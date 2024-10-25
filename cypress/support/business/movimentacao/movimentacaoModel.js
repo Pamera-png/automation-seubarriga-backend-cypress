7e19e07e6d038a400b409fbc163b639b24f77309
Pamera-png <pameranatali@gmail.com>
2024-10-25 13:17:48 -0300

    teste inserir movimentacao realizado com sucesso

--- cypress/support/business/movimentacao/movimentacaoModel.js
+++ cypress/support/business/movimentacao/movimentacaoModel.js
@@ -1,5 +1,13 @@
-let variavel=""
-class MovimentacaoPage{
+let token='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDA3NTh9.IPO0hq3HRiDPjHkGAaf7ns3oJTPPNb2BRQqKNWS-4fc'
+let contaByName="Conta para alterar"
 
+class MovimentacaoModel{
+getToken(){
+    return token
 }
-export default new MovimentacaoPage;
+
+getContaByName(){
+    return contaByName
+}
+}
+export default new MovimentacaoModel;
--- cypress/support/business/movimentacao/movimentacaoSteps.js
+++ cypress/support/business/movimentacao/movimentacaoSteps.js
@@ -2,5 +2,5 @@ import { Given, Then, When, And } from "cypress-cucumber-preprocessor/steps"
 import MovimentacaoLogic from '../movimentacao/movimentacaoLogic';
 
 When('insiro uma movimentacao com sucesso', ()=>{
-
+MovimentacaoLogic.criarMovimentacao()
 })
